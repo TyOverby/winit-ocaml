96fc4e385fef009bf179eb24895234fda4c0b88b
Ty Overby <ty@pre-alpha.com>
2026-01-31 14:33:20 -0500

    allow passing extra flags to test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 echo "Running test_ffi.exe"
-dune exec examples/test_ffi.exe
+dune exec examples/test_ffi.exe "$@"
 echo "done!"
 
 echo ""
 echo "Running \`dune runtest\`"
-dune runtest
+dune runtest "$@"
 echo "done!"
