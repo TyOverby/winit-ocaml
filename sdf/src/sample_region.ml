98150d1e16da60e9cd887178f7ed3b0454090a5c
Ty Overby <ty@pre-alpha.com>
2026-06-04 17:30:45 -0400

    remove unnecessary annotations

--- sdf/src/sample_region.ml
+++ sdf/src/sample_region.ml
@@ -7,31 +7,31 @@ type t =
   ; samples_y : int
   }
 
-let (step_x @ portable) t =
+let step_x t =
   (Float32_u.to_float t.end_x -. Float32_u.to_float t.start_x) /. Float.of_int t.samples_x
 ;;
 
-let (step_y @ portable) t =
+let step_y t =
   (Float32_u.to_float t.end_y -. Float32_u.to_float t.start_y) /. Float.of_int t.samples_y
 ;;
 
-let (x_at @ portable) t col =
+let x_at t col =
   if col = 0
   then t.start_x
   else Float32_u.of_float (Float32_u.to_float t.start_x +. (step_x t *. Float.of_int col))
 ;;
 
-let (y_at @ portable) t row =
+let y_at t row =
   if row = 0
   then t.start_y
   else Float32_u.of_float (Float32_u.to_float t.start_y +. (step_y t *. Float.of_int row))
 ;;
 
-let (row @ portable) t r =
+let row t r =
   let y = y_at t r in
   { t with start_y = y; end_y = y; samples_y = 1 }
 ;;
 
-let (point @ portable) ~x ~y =
+let point ~x ~y =
   { start_x = x; end_x = x; samples_x = 1; start_y = y; end_y = y; samples_y = 1 }
 ;;
